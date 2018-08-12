8af8501bb1a93106002457f6a0ae872ee2ad412a
martinjaymckee <martinjaymckee@gmail.com>
2018-08-12 10:36:35 -0600

    Struggled with videos streaming -- no joy.  Cleaned up initial interface code.

--- server.py
+++ server.py
@@ -8,8 +8,6 @@ import flask_socketio
 import gevent
 import gevent.pywsgi as wsgi
 
-import camera_source
-
 app = flask.Flask(__name__)
 app.debug = True
 app.config['SECRET_KEY'] = 'secret!'
@@ -18,18 +16,21 @@ bootstrap = flask_bootstrap.Bootstrap(app)
 mako = flask_mako.MakoTemplates(app)
 socketio = flask_socketio.SocketIO(app)
 
+counter = 0
+
 @app.route('/')
 def index():
 	return flask_mako.render_template('index.html', date=datetime.datetime.now())
 
 @socketio.on('move', namespace='/mrl')
 def move(cmd):
+	global counter
 	msg = {
 		'cmd' : 'move', # NOTE: THIS CAN BE SET
-		'id' : 0, # NOTE: THIS SHOULD BE BASED UPON THE ID IN THE COMMAND
+		'id' : counter%3, # NOTE: THIS SHOULD BE BASED UPON THE ID IN THE COMMAND
 		'completed' : True # TODO: BUILD A SIMULATED ROBOT AND ONLY RETURN COMPLETED STATUS ON SUCCESS
 	}
-	print('Move Command -> {}'.format(cmd))
+	counter += 1
 	return flask_socketio.emit('status', msg, broadcast=True)
 
 @socketio.on('connect', namespace='/mrl')
@@ -39,21 +40,6 @@ def onConnect():
 @socketio.on('disconnect', namespace='/mrl')
 def onDisconnect():
 	pass
-
-def gen(camera):
-	"""Video streaming generator function."""
-	while True:
-		frame = camera.get_frame()
-		yield (b'--frame\r\nContent-Type: image/png\r\n\r\n' + frame + b'\r\n')
-
-@app.route('/video_feed')
-def video_feed():
-	"""Video streaming route. Put this in the src attribute of an img tag."""
-	files = ['static/images/frame_{}.png'.format(i) for i in range(1,8)]
-	camera = camera_source.EmulatedCamera(files, 1.0/25.0)
-	while camera.get_frame() is None:
-		time.sleep(0)
-	return flask.Response(gen(camera), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
 	http_server = wsgi.WSGIServer(('', 5000), app)
--- static/js/controls.js
+++ static/js/controls.js
@@ -1,54 +1,103 @@
-var socket=Nil;
+var socket=null;
 var x;
 var y;
 var width;
 var height;
 var radius;
 var max_distance = 10;
+var canvas;
 
 $(document).ready(function(){
     socket = io.connect('http://localhost:5000/mrl');
-    // socket.on('message', function(msg) {
-    //     $('#messages').append('<p>Received: ' + msg.text + '</p>');
-    // });
-    // $('#send').click(function(event) {
-    //     socket.emit('message', { text : $('#msg').val()});
-    //     return false;
-    // });
+
+    $('#success-alert').hide();
+    $('#info-alert').hide();
+    $('#danger-alert').hide();
+
+    socket.on('status', function(status) {
+      if(status.id == 0) {
+        $('#success-alert').show();
+        $('#info-alert').hide();
+        $('#danger-alert').hide();
+      } else if (status.id == 1) {
+        $('#success-alert').hide();
+        $('#info-alert').show();
+        $('#danger-alert').hide();
+      } else {
+        $('#success-alert').hide();
+        $('#info-alert').hide();
+        $('#danger-alert').show();
+      }
+    });
+
 });
 
-function setup() {
-  width = 400; // TODO: THIS NEEDS TO BE TAKEN FROM THE DOCUMENT AND TREATED AS DYNAMIC
-  height = 400;
+function updateControlDimensions() {
+  height = windowHeight;
+  width = windowWidth;
+  width = 0.75 * Math.min(width, height);
+  height = width;
   radius = width/2;
-  var canvas = createCanvas(width, height);
+}
+
+function setup() {
+  updateControlDimensions();
+  canvas = createCanvas(width, height);
   canvas.parent('p5-controls');
   canvas.mousePressed( function() {
     // TODO: CONVERT FROM CARTESIAN TO POLAR
     var max_radius = radius - 10;
     var dx = mouseX - (width/2);
     var dy = mouseY - (height/2);
-    var r = Math.min(max_radius, Math.sqrt(dx*dx + dy*dy));
-    var alpha = Math.atan2(dy, dx);
-    var rads = alpha;
-    var power = 100;
-    x = r * Math.cos(rads) + (width/2);
-    y = r * Math.sin(rads) + (height/2);
-    console.log( "r = " + r + "x = " + dx + ", y = " + dy);
-    distance = (max_distance * r ) / max_radius;
-    cmd = {distance:distance, rads:rads, power:power};
-    socket.emit('move', cmd)
+    var r = Math.sqrt(dx*dx + dy*dy);
+    if(r <= max_radius) {
+      var alpha = Math.atan2(dy, dx);
+      var rads = alpha;
+      var power = 100;
+      x = r * Math.cos(rads) + (width/2);
+      y = r * Math.sin(rads) + (height/2);
+      // TODO: NEED TO MAKE THIS CALCULATE THE CORRECT "TURN" ANGLE
+      //console.log( "r = " + r + "x = " + dx + ", y = " + dy);
+      distance = (max_distance * r ) / max_radius;
+      cmd = {distance:distance, rads:rads, power:power};
+      console.log(cmd);
+      socket.emit('move', cmd);
+    } else {
+      // TODO: THIS IS NOT ON THE CONTROL GRID
+    }
   });
   background(0, 0, 0, 0);
+}
+
+function polygon(x, y, radius, npoints) {
+  var angle = TWO_PI / npoints;
+  beginShape();
+  for (var a = 0; a < TWO_PI; a += angle) {
+    var sx = x + cos(a) * radius;
+    var sy = y + sin(a) * radius;
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
 }
 
 function draw() {
   noStroke();
   // TODO: THE BACKGROUND SHOULD BE AN IMAGE
+  // TODO: DRAW AN ARROW AND, POTENTIALLY, FILL BASED ON PROGRESS
+  // TODO: THE CONTROLS SHOULD BE DRAWN HERE
   fill('blue');
   ellipse(width/2, height/2, width, height);
   fill('white');
   ellipse(x, y, 10);
   fill('yellow');
   ellipse(width/2, height/2, width/10, height/10);
+
+  fill('green');
+  poly = polygon(width/10, width/10, width/11, 3);
+}
+
+function windowResized() {
+  updateControlDimensions();
+  resizeCanvas(width, height);
+  background(0, 0, 0, 0);
 }
